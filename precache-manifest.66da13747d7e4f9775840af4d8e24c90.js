84d2a478ae6bf6f08688b8da00ebf4ff81bcb04d
tom reifenberg <reifenberg.57@gmail.com>
2020-09-17 12:13:09 -0500

    Updates

---
+++ precache-manifest.66da13747d7e4f9775840af4d8e24c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e4128ab1a8706802ee3ca63fdedee2",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "e60abaa0cf765c29c0fd",
+    "url": "/carousel/static/css/main.2a24fd39.chunk.css"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/carousel/static/js/2.93e32125.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel/static/js/2.93e32125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60abaa0cf765c29c0fd",
+    "url": "/carousel/static/js/main.2c2f1762.chunk.js"
+  },
+  {
+    "revision": "2334337016e041c9c1cf",
+    "url": "/carousel/static/js/runtime-main.9b8b9a58.js"
+  }
+]);
